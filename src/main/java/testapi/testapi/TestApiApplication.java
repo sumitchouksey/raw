0d559e8d6a0048ded7e0e1f4438ecca120dc3218
sumitchouksey <sumitchouksey2315@gmail.com>
2017-10-25 21:33:09 +0530

    Initial Commit

---
+++ src/main/java/testapi/testapi/TestApiApplication.java
@@ -0,0 +1,12 @@
+package testapi.testapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApiApplication.class, args);
+	}
+}
